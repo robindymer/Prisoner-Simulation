4b90d16cb4021177e463a2d5802dcc3b0504d9c8
Robin Dymér <robin.dymer@hotmail.com>
2022-07-14 01:11:57 +0200

    Shuffle buzzle

--- prison.cpp
+++ prison.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 // Function declarations
 int randint(int num);
+void swap(int *a, int *b);
 
 /* Describes the room where the prisoner enters */
 class Room
@@ -18,21 +19,64 @@ class Room
     Room(int bufSize);
     ~Room();
     void print();
-    void generateNumbers();
+    void shuffleNumbers();
+    int getNumber(int i);
     bool openBox();
 };
 
 /* Main program */
 int main()
 {
-  int bufSize = 100;
+  int bufSize = 10;
   // Create a room with 100 boxes
   // Each "box" (element in the array) has a unique number ranging
   // from 1-100
   Room room = Room(bufSize);
 
   // Test
-  room.print();
+  // room.print();
+
+  // Try to find number by following the loop
+  // Run the simulation n times
+  int n = 100;
+  int failed = 0;
+
+  for (int _ = 0; _ < n; _++)
+  {
+
+    room.shuffleNumbers();
+    
+    for (int i = 1; i <= bufSize; i++)
+    {
+      int attempts = 0;
+      int current = i;
+      bool survived = false;
+
+      while (attempts < bufSize / 2)
+      {
+        int next = room.getNumber(current-1);
+        attempts++;
+
+        if (next == i)
+        {
+          // cout << "Number found at attempt: " << attempts << endl;
+          survived = true;
+          break;
+        }
+        
+        current = next;
+      }
+
+      if (!survived)
+      {
+        cout << "Number not found!" << endl;
+        failed++;
+        break;
+      }
+    }
+
+  }
+  cout << "Rate of success: " << failed;
   return 0;
 }
 
@@ -68,13 +112,35 @@ void Room::print()
     cout << "Box contains the number: " << boxes[i] << endl;
 }
 
-// Make the sequence random
-void shuffleNumbers()
+int Room::getNumber(int i)
 {
-  //TODO: Implement
+  return boxes[i];
 }
 
 bool Room::openBox()
 {
-    return true;
+  return true;
+}
+
+// Make the sequence random
+void Room::shuffleNumbers()
+{
+  // Set the seed
+  srand(time(NULL));
+
+  // Swap one by one, starting with the last element
+  for (int i = bufSize - 1; i > 0; i--)
+  {
+    // Random index from 0 to i
+    int j = rand() % (i + 1);
+    swap(&boxes[i], &boxes[j]);
+  }
+}
+
+// Swap two integers
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
